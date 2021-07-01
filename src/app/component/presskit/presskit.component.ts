5f7fc9bf50ea2631c0af6814e59deefb2e5e6b28
lisams <lisa@schedlberger.net>
2021-07-01 19:08:02 +0200

    added screenshots

--- src/app/component/presskit/presskit.component.ts
+++ src/app/component/presskit/presskit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-presskit',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresskitComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) { }
 
   ngOnInit(): void {
+  }
+
+  getCurLang(): String{
+    return this.translate.currentLang
   }
 
 }
